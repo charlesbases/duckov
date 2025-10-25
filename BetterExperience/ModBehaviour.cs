c493173f496698ac089a78ce7e907341ce34c2f7
zhiming.sun <zhiming.sun@qq.com>
2025-10-25 19:22:31 +0800

    fix: 修复物品拆分时部分物品丢失

--- BetterExperience/ModBehaviour.cs
+++ BetterExperience/ModBehaviour.cs
@@ -1,4 +1,3 @@
-using System.Reflection;
 using Duckov.UI;
 using ItemStatsSystem;
 
@@ -8,30 +7,33 @@ namespace BetterExperience
     {
         void OnEnable()
         {
-            // 最大堆叠
-            ItemHoveringUI.onSetupItem += OnSetupItemSetStackCount;
-            // 枪械耐久
-            ItemHoveringUI.onSetupItem += OnSetupItemSetDurability;
+            ItemHoveringUI.onSetupItem += OnSetupItem;
         }
         
         void OnDisable()
         {
-            // 最大堆叠
-            ItemHoveringUI.onSetupItem -= OnSetupItemSetStackCount;
-            // 枪械耐久
-            ItemHoveringUI.onSetupItem -= OnSetupItemSetDurability;
+            ItemHoveringUI.onSetupItem -= OnSetupItem;
         }
-        
-        private void OnSetupItemSetStackCount(ItemHoveringUI uiInstance, Item item)
+
+        private void OnSetupItem(ItemHoveringUI uiInstance, Item item)
         {
+            // 修改物体
+            SetupItem(item);
+            // 修改预制体
+            SetupItem(ItemAssetsCollection.GetPrefab(item.TypeID));
+        }
+
+        private void SetupItem(Item item)
+        {
+            if (item == null)
+                return;
+            
+            // 堆叠数
             if (item.Stackable && item.MaxStackCount < 999)
             {
                 item.MaxStackCount = item.MaxStackCount < 99 ? 99 : 999;
             }
-        }
-        
-        private void OnSetupItemSetDurability(ItemHoveringUI uiInstance, Item item)
-        {
+            // 耐久度
             item.DurabilityLoss = 0;
         }
     }
